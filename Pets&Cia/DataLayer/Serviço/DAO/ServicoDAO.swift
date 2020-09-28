ddeb9a72c794dd8bc75d5a5be50b8a7bf9f438b6
Pedro Jaime <pedrojaimesjr@MacBook-Pro-de-Pedro.local>
2020-09-28 00:11:27 -0300

    Implementacao do ServicoDAO

---
+++ Pets&Cia/DataLayer/Serviço/DAO/ServicoDAO.swift
@@ -0,0 +1,36 @@
+//
+//  ServicoDAO.swift
+//  Pets&Cia
+//
+//  Created by Pedro Jaime on 27/09/20.
+//  Copyright © 2020 Jobson Mateus. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class ServicoDAO: DAO{
+    
+    typealias T = Servico
+    var realm: Realm
+    
+    public init(realm: Realm){
+        self.realm = realm
+    }
+    
+    func update(object: Servico, id: Any) {
+        
+        let result = self.getReference(by: id)
+        
+        try? self.realm.write{
+            result?.set(nome: object.nome)
+            result?.set(descricao: object.descricao)
+            result?.set(valor: object.valor.value)
+        }
+    }
+    
+    func setAutoIncrement(of object: Servico, with id: Any) {
+        
+        object.id.value = id as? Int
+    }    
+}
